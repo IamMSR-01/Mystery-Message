96a8ba302b5ea377cf18d8f3f6e0303a29ac5bca
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-08-22 17:28:07 +0530

    Add mongoose models and validation schemas for user messages
    - Introduced User and Message models using mongoose.
    - Added validation schemas for sign up, sign in, message acceptance, and verification using zod.
    - Updated package.json and package-lock.json to include mongoose and zod dependencies.

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { email, z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(3, "User must be atleast 3 character")
+  .max(20, "User does not contain more than 20 character")
+  .regex(/^[a-zA-Z0-9_]+$/, "User does not contain any special character")
+
+
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({message: "Invalid email address"}),
+    password: z.string().min(6, {message: "Password must be atleast 6 character"})
+})
