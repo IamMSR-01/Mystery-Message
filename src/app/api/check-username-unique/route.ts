4c372a944d567501976ec5bb0ea8cdb970d6d953
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-08-24 01:15:17 +0530

    feat: implement authentication and user verification flow with NextAuth and email verification

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,58 @@
+import dbConnect from "@/lib/db";
+import UserModel from "@/models/User";
+import { success, z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    // velidate with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+
+      return Response.json({
+        success: false,
+        message: usernameErrors?.length > 0 ? usernameErrors.join(', ') : "Invalid query parameter"
+      }, { status: 400 })
+    }
+
+    const { username } = result.data
+
+    const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true })
+
+    if (existingVerifiedUser) {
+      return Response.json({
+        success: false,
+        message: "username is already taken"
+      }, { status: 400 })
+    }
+
+    return Response.json({
+      success: true,
+      message: "This username is available"
+    }, { status: 200 })
+
+
+  } catch (error) {
+    console.error("Username checking error", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Usename checking error",
+      },
+      { status: 500 }
+    );
+  }
+}
