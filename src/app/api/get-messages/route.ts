f51b09b7a790cb97b820aedab0db31cfb52d597c
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-08-24 23:45:18 +0530

    feat: implement POST route for sending messages with user validation

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from "../auth/[...nextauth]/options";
 import UserModel from "@/models/User";
 import dbConnect from "@/lib/db";
 import { User } from "next-auth";
+import mongoose from "mongoose";
 
 
 export async function GET(request: Request) {
@@ -21,12 +22,43 @@ export async function GET(request: Request) {
                 { status: 401 }
             );
         }
-    
-        const userId = user._id;
+
+        const userId = new mongoose.Types.ObjectId(user._id);
 
         try {
-            
+            const user = await UserModel.aggregate([
+                { $match: { _id: userId } },
+                { $unwind: "$messages" },
+                { $sort: { "messages.createdAt": -1 } },
+                { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+            ])
+
+            if (!user || user.length === 0) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "User not found",
+                    },
+                    { status: 404 }
+                );
+            }
+
+            return Response.json(
+                {
+                    success: true,
+                    messages: user[0].messages,
+                },
+                { status: 200 }
+            );
+
         } catch (error) {
-            
+            console.log(error);
+            return Response.json(
+                {
+                    success: false,
+                    message: "Internal Server Error",
+                },
+                { status: 500 }
+            );
         }
 }
