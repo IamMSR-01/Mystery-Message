bd3e6e88d0a4dc785654cf81204f16cb80e4b589
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-08-27 01:41:59 +0530

    feat: update dependencies and add new UI components for form handling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,15 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+import * as z from "zod"
+import Link from "next/link";
 
-import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+
+function page() {
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-green-700 p-2 rounded px-3 font-semibold" onClick={() => signIn()}>Sign in</button>
-    </>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
