d0c98f4d566899fe6d9b2f49a4d18b40787d5315
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-09-02 12:08:39 +0530

    fix delete route bugs

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -6,9 +6,9 @@ import { User } from "next-auth";
 
 export async function DELETE(
     request: Request,
-    { params }: { params: { messageid: string } }
+    context: { params: { messageid: string } }
 ) {
-    const messageId = params.messageid;
+    const messageId = context.params.messageid;
     await dbConnect();
 
     const session = await getServerSession(authOptions);
